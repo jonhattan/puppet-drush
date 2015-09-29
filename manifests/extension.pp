07f5bf8af1dd887edc390f31f9557388991d05c9
Jonathan Araña Cruz <jonhattan@faita.net>
2015-09-29 16:59:21 +0100

    Adjust variable names.

--- manifests/extension.pp
+++ manifests/extension.pp
@@ -4,12 +4,12 @@ define drush::extension() {
     fail('You must include class drush before declaring aliases')
   }
 
-  # split $name at the dash to eliminate the version component
-  $name_arr = split($name, '-')
-  $folder_name = $name_arr[0]
+  # Split $name at the dash to eliminate the version component.
+  $parts = split($name, '-')
+  $extension_name = $parts[0]
 
   exec {"${drush::drush_exe_default} dl ${name}":
-    creates => "/usr/share/drush/commands/${folder_name}",
+    creates => "/usr/share/drush/commands/${extension_name}",
     notify  => Class['cacheclear'],
   }
 
