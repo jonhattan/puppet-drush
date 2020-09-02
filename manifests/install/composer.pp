jonhattan/puppet-drush

commit ed87127c0aa2ad560a54a6b224b54953b1f8ad9e

Actually run composer only if the version changed.

manifests/install/composer.pp
@@ -32,6 +32,7 @@ module and should not be directly included in the manifest.")
     cwd         => $install_path,
     environment => ["COMPOSER_HOME=${composer_home}"],
     require     => File[$install_path],
+    onlyif      => "test \"$(grep drush/drush composer.json | cut -d\" -f 4)\" != '${real_version}'"
   }
 
 }

commit f155936b4d5a457ff19d5ec2cbdbb6cc0d1409f8

Fix a bug on new installations where the exec doesn't run (regression in ed87127c0aa2ad560a54a6b224b54953b1f8ad9e).

manifests/install/composer.pp
@@ -32,7 +32,7 @@ module and should not be directly included in the manifest.")
     cwd         => $install_path,
     environment => ["COMPOSER_HOME=${composer_home}"],
     require     => File[$install_path],
-    onlyif      => "test \"$(grep drush/drush composer.json | cut -d\" -f 4)\" != '${real_version}'"
+    onlyif      => "test ! -f composer.json || test \"$(grep drush/drush composer.json | cut -d\\\" -f 4)\" != '${real_version}'"
   }
 
 }