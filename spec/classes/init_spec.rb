04971354380150a8d41059a9a03c2c13ad22e099
Jonathan Araña Cruz <jonhattan@faita.net>
2023-12-22 11:06:45 +0100

    Update to pdk 3.0.0

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 describe 'drush' do
-
   context 'with defaults for all parameters' do
-    it { should contain_class('drush') }
+    it { is_expected.to contain_class('drush') }
   end
 end
